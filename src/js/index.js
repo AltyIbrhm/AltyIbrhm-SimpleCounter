81517d4de3af147da278e3d11e0145e98af0cf44
AltyIbrhm <alty.ibrhm@gmail.com>
2024-10-16 20:39:36 -0400

    timer

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import PropTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -8,6 +9,15 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+let counter = 0 ;
+
+setInterval(function(){
+   root.render(<Home counter={counter}/>);
+    counter++;
+}  , 1000)
+
+
+
 
